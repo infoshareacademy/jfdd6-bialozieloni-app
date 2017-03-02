0b781c0258cd93c4c2e5a52ea523c708e24e83fe
Robert Papiernik <robertpapiernik@o2.pl>
2017-03-02 17:04:52 +0100

    wip

--- src/components/chart-view.js
+++ src/components/chart-view.js
@@ -1,11 +1,34 @@
 import React from 'react'
-import data from './chart-data'
+import data from './chart-data.json'
 import { Line } from 'react-chartjs'
 
-const ChartView = () => (
-  <div>
-    <Line data={data.data} width="1000" height="400"/>
-  </div>
-)
+class ChartView extends React.Component {
+  constructor(props) {
+    super(props)
 
+    this.state = {
+    data: data
+    }
+
+    setInterval(() => {
+      this.setState({
+        data: {
+          labels: this.state.data.labels,
+          datasets: this.state.data.datasets.map(
+            dataset => ({
+              ...dataset,
+              data: dataset.data.slice(1).concat(Math.random()*50)
+            }),
+          )}
+      })
+    }, 1000)
+}
+  render() {
+    return (
+        <div>
+          <Line data={this.state.data} width="1000" height="400"/>
+        </div>
+    )
+  }
+}
 export default ChartView
