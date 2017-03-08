e4c904ce94a4c3922d04a9bc4995fc139f2333c3
Jan Radzikowski <jan.s.radzikowski@gmail.com>
2017-03-08 13:08:51 +0100

    FD6BZ-27 BANK.js added table

--- src/components/BANK.js
+++ src/components/BANK.js
@@ -1,8 +1,38 @@
 import React from 'react'
-import {Grid,} from 'react-bootstrap'
+import {Grid, Table} from 'react-bootstrap'
 
 const Bank = () => (
 <Grid>
+  <Table striped>
+    <thead>
+    <tr>
+      <th>Oferta</th>
+      <th>Ilosc</th>
+      <th>Walor</th>
+      <th>Limit ceny</th>
+    </tr>
+    </thead>
+    <tbody>
+    {
+      groups.data ?
+        groups.data.map(
+          group => (
+            <tr key={group.id}>
+              <td>{group.id}</td>
+              <td>{group.name}</td>
+              <td>{group.studentIds.length}</td>
+              <td>{group.studentIds.length}</td>
+            </tr>
+          )
+        ) :
+        <tr>
+          <td colSpan="3">
+            Waiting for groups...
+          </td>
+        </tr>
+    }
+    </tbody>
+  </Table>
 <p> Oferta:</p>
 <p>Ilosc: </p>
 <p>Walor: </p>
