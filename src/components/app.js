26f9192264a6b47e0e5f68ffb778159b1b3098cb
Tomasz Dudek <tdkontakt@gmail.com>
2017-03-08 16:03:45 +0100

    przenieśliśmy action creatora do kaczki

--- src/components/app.js
+++ src/components/app.js
@@ -1,32 +1,41 @@
 import React from 'react'
+
 import {Navbar, Nav, NavItem} from 'react-bootstrap'
 import {LinkContainer} from 'react-router-bootstrap'
 import {Link} from 'react-router'
 
-const App = (props) => (
-  <div>
-    <Navbar>
-      <Navbar.Header>
-        <Navbar.Brand>
-          <Link to="/">
-            Home
-          </Link>
-        </Navbar.Brand>
-        <Navbar.Toggle />
-      </Navbar.Header>
-      <Navbar.Collapse>
-        <Nav>
-          <LinkContainer to="/bank-form">
-            <NavItem eventKey={3}>Bank-form</NavItem>
-          </LinkContainer>
-          <LinkContainer to="/wig-20">
-            <NavItem eventKey={3}>WIG20</NavItem>
-          </LinkContainer>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-    {props.children}
-  </div>
-)
+class App extends React.Component {
+  componentWillMount() {
+
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to="/">
+                Home
+              </Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav>
+              <LinkContainer to="/bank-form">
+                <NavItem eventKey={3}>Bank-form</NavItem>
+              </LinkContainer>
+              <LinkContainer to="/wig-20">
+                <NavItem eventKey={3}>WIG20</NavItem>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        {this.props.children}
+      </div>
+    )
+  }
+}
 
 export default App
--- src/store.js
+++ src/store.js
@@ -1,17 +1,22 @@
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+//import persistState from 'redux-localstorage'
 
-import Wig20Reducer from './components/wig20-reducer'
-
+import Wig20Reducer from './state/wig20-reducer'
 
 const reducer = combineReducers({
-  sample: (state = {}, action = {}) => state,
   bankData: (state = {}, action = {}) => state,
   companies: Wig20Reducer
 })
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+  /* preloadedState, */
+  composeEnhancers(
+    //persistState(['counter']),
+    applyMiddleware(thunk)
+  )
+);
 
 export default store
