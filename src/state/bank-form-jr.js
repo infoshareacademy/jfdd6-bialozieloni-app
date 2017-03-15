8f30f777eb6d1a2a925514677f0a07f16cda4005
Jan Radzikowski <jan.s.radzikowski@gmail.com>
2017-03-15 17:14:03 +0100

    FD6BZ-27 adding inormation about acceptance in transaction history work still in progress

--- src/state/bank-form-jr.js
+++ src/state/bank-form-jr.js
@@ -11,9 +11,17 @@ const formReducer = (state = initialState, action = {}) => {
         transactions: state.transactions.concat({...action.transaction, isAccepted: false})
       }
     case 'bankForm/ACCEPT_NEXT':
+
       return {
         ...state,
-        transactions: state.transactions
+        transactions: state.transactions.map((item, index, array)=> {
+          if (index === array.length - 1) {
+            for (var i = array.length - 1; i >= 0; i -= 1) {
+              return {...item, isAccepted: true}
+            }
+          }
+          return item
+        })
       }
 
     default:
